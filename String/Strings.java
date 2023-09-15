9567cc3b22a8cf91ef049dfc236d71cf9875f3de
Gaurav Thalkar <134489977+thalkargaurav@users.noreply.github.com>
2023-09-15 22:57:43 +0530

    String

---
+++ String/Strings.java
@@ -0,0 +1,9 @@
+package com.jsp.strings;
+
+public class Strings {
+	public static void main(String[] args) {
+		String s1="hello";
+		String s2="hello";
+		System.out.println(s1==s2);
+	}
+}
---
+++ String/StringsEg2.java
@@ -0,0 +1,12 @@
+package com.jsp.strings;
+
+public class StringsEg2 {
+	public static void main(String[] args) {
+		String s1=new String("hello");
+		String s2="hello";
+		String s3="hello";
+		System.out.println(s1==s2);
+		System.out.println(s2==s3);
+	}
+
+}
